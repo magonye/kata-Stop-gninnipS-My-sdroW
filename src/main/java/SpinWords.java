b9a3807d59fd14cc9622bae99917993dc6b450da
magonye <magonye@gmail.com>
2017-02-24 18:34:09 +0100

    first TDD Iteration

---
+++ src/main/java/SpinWords.java
@@ -0,0 +1,9 @@
+/**
+ * Created by magonye on 24/2/17.
+ */
+public class SpinWords {
+    public String spinWords(String welcome) {
+        return welcome;
+    }
+
+}
---
+++ src/test/java/SpinWordsShould.java
@@ -0,0 +1,20 @@
+/**
+ * Created by magonye on 24/2/17.
+ */
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SpinWordsShould {
+
+    @Test public void
+    return_a_word_without_reversing_when_less_than_five_letters() {
+        assertEquals("one",new SpinWords().spinWords("one"));
+    }
+
+    //@Test
+    //public void test() {
+    ///    assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
+    //    assertEquals("Hey wollef sroirraw", new SpinWords().spinWords("Hey fellow warriors"));
+    //}
+
+}
